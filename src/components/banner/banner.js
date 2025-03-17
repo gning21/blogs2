4d9abd5313a627b44064ff0d79174109cf6fdecc
gning21 <12353631+gning21>
2025-03-18 03:05:39 +0800

    Initial commit for blogs2

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -6,7 +6,7 @@
  * @describe: banner 处理
  */
 import bannerTemp from '../../template/banner.html';
-import homeTopBg from "../../images/webp/home_top_bg.webp";
+import homeTopBg from "../../images/webp/hhh.webp";
 import netHomeTopBg from "../../images/webp/nothome_top_bg.webp";
 
 export default function main(_) {
